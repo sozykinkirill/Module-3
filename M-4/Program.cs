934a3085da01ac005e23b8f600ffe06dfe429dce
sozykinkirill <kirill.sozykin159@yandex.ru>
2023-03-14 11:18:09 +0500

    4.1.18

--- M-4/Program.cs
+++ M-4/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace M_4
+﻿using System;
+
+namespace M_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Write your favorite color in English with a small letter:");
+            switch (Console.ReadLine())
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your favorite colir is red");
+                    break;
+                case "grean":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your favorite colir is green");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your favorite color is cyan");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Your favorite color is yellow");
+                    break;
+            }
+
+           
         }
     }
 }
